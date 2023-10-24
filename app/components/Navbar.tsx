5bcecfb970b5100e0ab852f7bc396da235e491b8
Sourab Patil <sourab054@gmail.com>
2023-10-24 17:35:42 +0530

    Deployment commit

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,17 +1,21 @@
 import Image from "next/image";
 import Link from "next/link";
 
+import { getCurrentUser } from "@/lib/session";
+
 import AuthProviders from "./AuthProviders";
+import Button from "./Button";
+import ProfileMenu from "./ProfileMenu";
 import { NavLinks } from "../constant";
 
 const Navbar = async () => {
-  const session = {};
+  const session = await getCurrentUser();
 
   return (
     <nav className="flexBetween navbar">
       <div className="flex-1 flexStart gap-10">
         <Link href="/">
-          <Image src="/logo.svg" width={116} height={43} alt="logo" />
+          <Image src="/logo.png" width={120} height={43} alt="logo" />
         </Link>
         <ul className="xl:flex hidden text-small gap-7">
           {NavLinks.map((link) => (
@@ -23,7 +27,17 @@ const Navbar = async () => {
       </div>
 
       <div className="flexCenter gap-4">
-        {session ? <>Share Work</> : <AuthProviders />}
+        {session?.user ? (
+          <>
+            <ProfileMenu session={session} />
+
+            <Link href="/create-project">
+              <Button title="Share work" />
+            </Link>
+          </>
+        ) : (
+          <AuthProviders />
+        )}
       </div>
     </nav>
   );
