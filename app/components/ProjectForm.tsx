4671c94c8dd7e51e123f3a59fa5ab194a69e55a5
Sourab Patil <sourab054@gmail.com>
2023-11-03 20:36:04 +0530

    Fix: placeholders fixed

--- app/components/ProjectForm.tsx
+++ app/components/ProjectForm.tsx
@@ -131,7 +131,7 @@ const ProjectForm = ({ type, session, project }: Props) => {
         type="url"
         title="Website URL"
         state={form.liveSiteUrl}
-        placeholder="https://jsmastery.pro"
+        placeholder="https://codecanvas.com"
         setState={(value) => handleStateChange("liveSiteUrl", value)}
       />
 
@@ -139,7 +139,7 @@ const ProjectForm = ({ type, session, project }: Props) => {
         type="url"
         title="GitHub URL"
         state={form.githubUrl}
-        placeholder="https://github.com/adrianhajdin"
+        placeholder="https://github.com/codecanvas"
         setState={(value) => handleStateChange("githubUrl", value)}
       />
 
